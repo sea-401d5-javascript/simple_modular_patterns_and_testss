09e130cad5a7bd0875e723dec960fc60ebe02958
David Swaintek <dswaintek@gmail.com>
2016-05-09 20:46:16 -0700

    Add command line functionality, first bonus

--- swaintek/greet.js
+++ swaintek/greet.js
@@ -3,3 +3,7 @@ exports = module.exports = {};
 exports.hello = function (name) {
   return ('Hello ' + name);
 };
+
+if (process.argv[2] != 'undefined') {
+  console.log('Hello ' + process.argv[2]);
+};
