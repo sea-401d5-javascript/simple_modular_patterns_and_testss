4848a1cf0d53321e8a16b8438f9ee251a3cfb078
Stephen Schroeder <schrode50@gmail.com>
2016-05-09 22:50:18 -0700

    got it mostly working with 1 test successful.

--- Stephen_Schroeder/test/greetTest.js
+++ Stephen_Schroeder/test/greetTest.js
@@ -1,10 +1,10 @@
 const expect = require('chai').expect;
-const greeting = require(__dirname + '/../lib/greeter');
+const greeting = require(__dirname + '/../lib/greeting');
 const greet = greeting.greet;
 const greetMe = greeting.greetMe;
 
-// describe('It\'s a greet function', () => {
-//   it('should log greeting', () => {
-//     expect(greet('larry')).to.equal('hello larry');
-//   });
-// });
+describe('It\'s a greet function', () => {
+  it('should log greeting', () => {
+    expect(greet('larry')).to.equal('hello larry');
+  });
+});
