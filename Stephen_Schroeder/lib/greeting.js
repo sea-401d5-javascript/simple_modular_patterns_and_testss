8f8982c3255f9ba2c015c6962892bc3f3af959ca
Stephen Schroeder <schrode50@gmail.com>
2016-05-09 22:17:35 -0700

    most of the assignment structured out

---
+++ Stephen_Schroeder/lib/greeting.js
@@ -0,0 +1,3 @@
+exports.greet = name => 'hello ' + name;
+
+exports.greetMe = args => exports.greet(args[2]);
---
+++ Stephen_Schroeder/test/greetTest.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+const greeting = require(__dirname + '/../lib/greeter');
+const greet = greeting.greet;
+const greetMe = greeting.greetMe;
+
+// describe('It\'s a greet function', () => {
+//   it('should log greeting', () => {
+//     expect(greet('larry')).to.equal('hello larry');
+//   });
+// });
