0f1f909703caca7fff8d4da722d7708585f9953a
David Swaintek <dswaintek@gmail.com>
2016-05-10 16:45:33 -0700

    add watch task for mocha and lint

--- swaintek/gulpfile.js
+++ swaintek/gulpfile.js
@@ -2,14 +2,18 @@ var gulp = require ('gulp');
 var mocha = require('gulp-mocha');
 var eslint = require('gulp-eslint')
 
-gulp.task('default', function () {
+gulp.task('mocha', function () {
   return gulp.src(['test/greet_test.js'], {read: false})
     .pipe(mocha());
-})
+});
 
 gulp.task('lint', function () {
   return gulp.src(['**/*.js', '!node_modules'])
     .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-})
+    .pipe(eslint.format());
+});
+
+gulp.task('default', ['mocha', 'lint'], function () {
+  gulp.watch(['test/greet_test.js'], ['mocha']);
+  gulp.watch(['**/*.js'], ['lint']);
+});
