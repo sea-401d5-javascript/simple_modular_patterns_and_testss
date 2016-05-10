465218bead191828d7673caeb73451c119ea4588
Jeff Eng <jeff@jeffeng.me>
2016-05-10 11:28:10 -0700

    created an object with greet function attached. exported greet_module file. created test with bob passed in as an argument and passed

---
+++ jeff_eng/test/greet_test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const expect = chai.expect;
+const greetModule = require(__dirname + '/../greet_module.js');
+
+describe('greet test', () => {
+  it('the greet function should return when bob is passed in as an argument', () => {
+    expect(greetModule.greet('bob')).to.eql('hello bob');
+  });
+});
