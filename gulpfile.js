a9ba6f9edeeafb073d0daef905ae21157c650e0f
Dan Stineback <dstineback@gmail.com>
2016-05-11 10:26:46 -0700

    added gulp watch dep

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,13 @@ gulp.task('default', ['lint','test'], function (){
 });
 
 gulp.task('lint', () => {
-  gulp.src(['./**/*.js','!./node_modules/*.js'] )
+  gulp.src(['./simple-object-test/**/*.js','./gulpfile.js'] )
   .pipe(eslint())
   .pipe(eslint.format())
 });
 gulp.task('test', () => {
-  return gulp.src('./simple-object-test/object-test/test.js', {read: false})
-    .pipe(mocha({reporter: 'nyan'}));
+  return gulp.src('./simple-object-test/object-test/test.js')
+    .pipe(mocha());
 });
 
 gulp.task('watch', () => {
--- simple-object-test/object.js
+++ simple-object-test/object.js
@@ -6,16 +6,3 @@ exports.age = '25';
 exports.greet = function (name){
   return 'hello ' + name;
 };
-///class review below
-// 'use strict'
-// const greet = module.exports = function() {
-//   let name = "dave";
-//   if(process.argv[2]) name = process.argv[2];
-//
-//   or
-//
-//   let name =process.argv[2] || 'Dave';
-//
-//   return 'Hi ' + name;
-// };
-// console.log(greet());
