cc6a6a3b98ee6ea8862cfe16d10401a2fce4ac1f
Aliza Pilisuk <apilisuk@gmail.com>
2016-05-11 09:52:12 -0700

    fixed gulpfile

--- aliza/gulpfile.js
+++ aliza/gulpfile.js
@@ -1,3 +1,18 @@
 const gulp = require('gulp');
-const lint = require('eslint');
+const eslint = require('gulp-eslint');
 const mocha = require ('gulp-mocha');
+
+gulp.task('default', ['lint', 'mocha'], () => {
+  console.log('default for lint and mocha');
+});
+
+gulp.task('lint', () => {
+  gulp.src('greet.js')
+    .pipe(eslint({}))
+    .pipe(eslint.format())
+});
+
+gulp.task('mocha', () => {
+	return gulp.src('test/test.js', {read: false})
+		.pipe(mocha({reporter: 'nyan'}));
+});
