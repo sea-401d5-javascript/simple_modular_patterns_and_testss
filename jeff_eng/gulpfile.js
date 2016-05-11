60d527bb46cc16ad34dc0a640828a791ee2db3da
Jeff Eng <jeff@jeffeng.me>
2016-05-11 11:00:46 -0700

    updated gulpfile to include a default which runs both my test and eslint

--- jeff_eng/gulpfile.js
+++ jeff_eng/gulpfile.js
@@ -2,13 +2,17 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 
+gulp.task('default', ['mochachai', 'eslint'], () => {
+  console.log('Runs all the tests');
+});
+
 gulp.task('mochachai', () => {
   gulp.src('./test/greet_test.js')
     .pipe(mocha());
 });
 
 gulp.task('eslint', () => {
-  gulp.src('./greet_module.js');
-    .pipe(eslint());
+  gulp.src('./greet_module.js')
+    .pipe(eslint())
     .pipe(eslint.format());
 });
